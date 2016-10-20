034f2bdc3f20a7f18f52a758f9de65b9d229601c
mikkaz <mikkaz@st.amu.edu.pl>
2016-10-20 10:53:19 +0200

    projekt unity
    projekt

---
+++ Crane/Assets/rotate.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class rotate : MonoBehaviour {
+
+	string name="imie2";
+	// Use this for initialization
+	void Start () {
+		bool contain=name.Contains ("imie");
+		Debug.Log(contain);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
