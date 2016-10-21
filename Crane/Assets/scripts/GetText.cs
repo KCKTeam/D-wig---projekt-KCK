ad5da3fffe242a5e675ce41613f4d475b9cf1b47
mikkaz <mikkaz@st.amu.edu.pl>
2016-10-21 16:16:21 +0200

    Kilka poprawek

---
+++ Crane/Assets/scripts/GetText.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+public class GetText : MonoBehaviour {
+	InputField wejscie;
+	void Start(){
+		wejscie = GetComponent<InputField> ();
+	}
+
+	public void text(){
+		string text = wejscie.text; //zmienna text przechowuje polecenie wpisane w Unity
+		string[] podzielText = text.Split (' '); //dzieli tekst na slowa
+		for (int i = 0; i < podzielText.Length; i++) {
+			Debug.Log (podzielText [i]);
+		}
+		//-------kilka przydatnych funkcji------//
+		//text.Trim(); //usuwa spacje ze stringa
+		//text.Contains("slowo_do_odnalezienia");
+		//text.CompareTo("slowo_do_porownania");
+	}
+}
