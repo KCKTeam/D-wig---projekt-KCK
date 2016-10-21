f0dab0113646a41be787ef0150726859f223685b
mikkaz <mikkaz@st.amu.edu.pl>
2016-10-21 15:54:17 +0200

    Aktualizacja projektu

---
+++ Crane/Assets/GetText.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+public class GetText : MonoBehaviour {
+	InputField wejscie;
+	void Start(){
+		wejscie = GetComponent<InputField> ();
+	}
+
+	public void text(){
+		Debug.Log (wejscie.text);
+		string ab = wejscie.text;
+		Debug.Log (ab + "123");
+		//int stopnie = (int)wejscie.text;
+	}
+}
