074b9b9e62adc82d8bba531eaf96929ee5a8fc88
mikkaz <mikkaz@st.amu.edu.pl>
2016-11-03 13:37:40 +0100

    Update

---
+++ Crane/Assets/scripts/CameraController.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+	Vector3 rotationPoint;
+	Vector3 lookAtPoint;
+	Vector3 direction;
+	float delta=80f;
+	float rotationSpeed=80f;
+	// Use this for initialization
+	void Start () {
+		rotationPoint =  new Vector3 (20f, 0, 0f);
+		lookAtPoint = new Vector3 (20f, 10f, 0f);
+		direction = Vector3.zero;
+	}
+	
+	// Update is called once per frame
+	float newYposition;
+	void Update () {
+		userInput ();
+		transform.RotateAround (rotationPoint, direction, rotationSpeed * Time.deltaTime);
+		newYposition = transform.position.y + delta * Time.deltaTime;
+		transform.position = new Vector3 (transform.position.x, newYposition, transform.position.z);
+		transform.LookAt (lookAtPoint);
+	
+	}
+
+
+	void userInput()
+	{
+		if (Input.GetKey (KeyCode.LeftArrow)) {
+			direction = Vector3.up;
+		} else if (Input.GetKey (KeyCode.RightArrow)) {
+			direction = Vector3.down;
+		}else
+			direction = Vector3.zero;
+
+		if (Input.GetKey (KeyCode.UpArrow)) {
+			if (transform.position.y > 2f) {
+				delta = -80f;
+			}else
+				delta = 0f;
+		} else if (Input.GetKey (KeyCode.DownArrow)) {
+			delta = 80f;
+		} else
+			delta = 0f;
+
+	}
+}
--- Crane/Assets/scripts/GetText.cs
+++ Crane/Assets/scripts/GetText.cs
@@ -5,47 +5,45 @@ using System.Text.RegularExpressions;
 
 
 public class GetText : MonoBehaviour {
+	
+	//tablica przechowująca wszystkie obiekty ze sceny
 	public GameObject [] obiekty;
-	string [] kolory={"ziel","niebi","czerw", "fiolet", "żółt", "biał", "czarn"};
-	string[] rodzaje = {"becz", "kontene", "skrzyn","samoch", "karto", "drzwia", "okn", "pił" };
-	string[] czas_pod = {"podnieś", "unieś"};
-	string [] czas_kladz={"opuść", "postaw","odłóż", "połóż"};
-	string[] zaimki = {"obok", "lew","praw", "na", "przed", "za"};
+
+	//słowa kluczowe
+	string [] kolory = {"ziel","niebi","czerw", "fiolet", "żółt", "biał", "czarn"};
+	string [] rodzaje = {"becz", "kontene", "skrzyn","samoch", "karto", "drzwia", "okn", "pił" };
+	string [] czas_pod = {"podnieś", "unieś"};
+	string [] czas_kladz = {"opuść", "postaw","odłóż", "połóż"};
+	string [] zaimki = {"obok", "lew","praw", "na", "przed", "za"};
 
 	InputField wejscie;
 	void Start(){
+		//wczytanie wszystkich obiektów do tablicy
 		obiekty = GameObject.FindGameObjectsWithTag ("obiekt");
 		for (int i = 0; i < obiekty.Length; i++) {
 			Debug.Log (obiekty [i].GetComponent<objectProperties> ().rodzaj);
 		}
+
 		wejscie = GetComponent<InputField> ();
 	}
-
+		
 	public void text(){
 		string text = wejscie.text; //zmienna text przechowuje polecenie wpisane w Unity
 		searchOrder(text);
-		/*
-		string[] podzielText = text.Split (' '); //dzieli tekst na slowa
-		for (int i = 0; i < podzielText.Length; i++) {
-			Debug.Log (podzielText [i]);
-		}
-
-		//-------kilka przydatnych funkcji------//
-		//text.Trim(); //usuwa spacje ze stringa
-		//text.Contains("slowo_do_odnalezienia");
-		//text.CompareTo("slowo_do_porownania");*/
 	}
+
+
 	bool czy_trzymam=false;
 	void searchOrder(string pobrany_tekst){
-		Debug.Log (pobrany_tekst);
 		bool obrot = false, czy_liczba = false;
 		bool czy_podnoszenie = false, czy_kladzenie = false;
 		string kierunek = "";
 		float kat = 0;
+		//zmienne do przetworzenia tekstu
 		char delimiter = ' ';
 		string[] substrings = pobrany_tekst.ToLower ().Split (delimiter);
 
-		//obracanie dźwigu
+		//----------------------obracanie dźwigu---------------------//
 		foreach (string substring in substrings) {
 			if (substring == "obróć") {
 				obrot = true;
@@ -63,7 +61,17 @@ public class GetText : MonoBehaviour {
 			}
 		}
 
-		//wyszukiwanie czasowników podnoszenia i opuszczania
+		if (obrot && czy_liczba) {
+			if (kierunek == "lewo") {
+				kat *= -1;
+			}
+
+			Debug.Log ("Obracam dźwig o "+ kat.ToString() +" stopni");
+			StartCoroutine(CraneManager.Instance.rotation(kat));
+		}
+		//--------------------------------------------------------------//
+
+		//----wyszukiwanie czasowników podnoszenia i opuszczania----//
 		foreach (string substring in substrings) {
 			for (int i = 0; i < czas_pod.Length; i++) {
 				if (substring.Contains (czas_pod [i])) {
@@ -79,7 +87,9 @@ public class GetText : MonoBehaviour {
 				}
 			}
 		}
+		//---------------------------------------------------------//
 
+		//----sprawdzenie, którą funkcję należy wywołać----//
 		if (czy_podnoszenie && czy_kladzenie) {
 			Debug.Log ("Podnosze i klade");
 		}else if ((czy_podnoszenie) && (!czy_trzymam)) {
@@ -88,8 +98,10 @@ public class GetText : MonoBehaviour {
 		}else if (czy_kladzenie && czy_trzymam) {
 			Debug.Log ("Tylko klade");
 		}
+		//------------------------------------------------//
+
 			
-		//
+		//----sprawdzenie koloru, rodzaju i zaimków---------//
 		foreach (string substring in substrings) {
 			for (int i = 0; i < kolory.Length; i++) {
 				if (substring.Contains (kolory [i])) {
@@ -106,20 +118,12 @@ public class GetText : MonoBehaviour {
 
 			for (int i = 0; i < zaimki.Length; i++) {
 				if (substring.Contains (zaimki [i])) {
-					Debug.Log ("Twój rodzaj: " + substring);
+					Debug.Log ("Twój zaimek: " + substring);
 				}
 			}
 		}
+		//------------------------------------------------------//
 			
-
-		if (obrot && czy_liczba) {
-				if (kierunek == "lewo") {
-					kat *= -1;
-				}
-			
-			Debug.Log ("Obracam dźwig o "+ kat.ToString() +" stopni");
-			StartCoroutine(CraneManager.Instance.rotation(kat));
-		}
 	}
 }
 	
